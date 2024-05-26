70500fab628c12675bc085239faee374fc3c0850
Aalok Pandit <51901277+fellowpandit@users.noreply.github.com>
2024-05-26 17:22:16 +0530

    Update p3b.c

--- p3b.c
+++ p3b.c
@@ -2,50 +2,49 @@
 
 #include<stdio.h>
 #include<string.h>
+#define size 50
 
 int main(){
-	int size=50;
 	char ptext[size], ctext[size+9], key[9];
 	
-	printf("Enter the key :");
-	scanf("%s",&key);
-	printf("Enter the plain text :");
-	scanf("%s",&ptext);
+	printf("Enter the key: ");
+	scanf("%s", key);
+	printf("Enter the text: ");
+	scanf("%s", ptext);
 	
 	int ptext_len = strlen(ptext);
 	int key_len = strlen(key);
 	
 	//Padding
-	int n=ptext_len%key_len;
-	n = n>0?key_len-n:0;
+	int n = ptext_len%key_len;
+	n = n>0? key_len-n: 0;
 	for(int i=0; i<n; i++){
-		ptext[ptext_len+i] =('x'+i-'a')%26+'a';
+		ptext[ptext_len++] = ('x'+i-'a')%26+'a';
 	}
-	ptext_len+=n;
-	ptext[ptext_len]='\0';
+	ptext[ptext_len] = '\0';
 	
-	//Finding the order of keys
+	//Order finding
 	int order_of_key[key_len];
-	for(int i=0; i<key_len; i++) {
-        for(int j=0; j<key_len; j++) {
-        	if(i==key[j]-'0'-1){
-        		order_of_key[i]=j;
-        		break;
-        	}
-    	}
-    }
-    
-	//Using positions to generate final text
+	for(int i=0; i<key_len; i++){
+		for(int j=0; j<key_len; j++){
+			if(i==key[j]-'0'-1){
+				order_of_key[i] = j;
+				break;
+			}
+		}
+	}
+	
+	//Cipher text creation
 	int pos=0;
-    for(int i=0; i<key_len; i++){
-    	int k=order_of_key[i];
-    	while(k<ptext_len){
-    		ctext[pos]=ptext[k];
-    		pos++;
-    		k+=key_len;
-    	}
-    }
-    
-    ctext[ptext_len] = '\0';
-	printf("%s",ctext);
+	for(int i=0; i<key_len; i++){
+		int chr_pos = order_of_key[i];
+		while(chr_pos<ptext_len){
+			ctext[pos] = ptext[chr_pos];
+			chr_pos+=key_len;
+			pos++;
+		}
+	}
+	
+	ctext[ptext_len] = '\0';
+	printf("Cipher Text: %s",ctext);
 }
